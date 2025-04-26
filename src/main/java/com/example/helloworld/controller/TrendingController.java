865dac63b6acba92d3ef1b3f1a9eb35a66cc836e
samuel077 <s750716@gmail.com>
2025-04-26 22:35:35 +0900

    remove .env

--- src/main/java/com/example/helloworld/controller/TrendingController.java
+++ src/main/java/com/example/helloworld/controller/TrendingController.java
@@ -31,7 +31,7 @@ public class TrendingController {
 
     @GetMapping("/hello-ci")
     public String ciHint() {
-        return "Hello, CI, v2, with CD to EC2, today";
+        return "Hello, CI, it's Sam.";
     }
 
     @GetMapping("/refresh")
