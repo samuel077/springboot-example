b59918f189defc0d8d1b14ce0f41cded7c98bafa
samuel077 <s750716@gmail.com>
2025-05-08 03:25:16 +0900

    Update corps setting

--- src/main/java/com/example/trending/config/CorsConfig.java
+++ src/main/java/com/example/trending/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOriginPatterns(
+                                "http://localhost:5173",
+                                "https://shuyu-lin.com"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("Authorization", "Content-Type")
                         .allowCredentials(true); // 如果前端有使用 credentials
